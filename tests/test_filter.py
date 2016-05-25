b87b28c5764d38eefc3728e06f6a6ad3c4978d56
Tony Narlock <tony@git-pull.com>
2016-05-25 07:14:03 -0500

    filter to pytest

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -5,149 +5,153 @@ from prompt_toolkit.filters.utils import to_cli_filter, to_simple_filter
 from prompt_toolkit.filters.cli import HasArg, HasFocus, HasSelection
 
 import unittest
+import pytest
 
 
-class FilterTest(unittest.TestCase):
-    def test_condition_filter_args(self):
-        c = Condition(lambda a, b, c:True)
-        self.assertTrue(c.test_args('a', 'b', 'c'))
-        self.assertFalse(c.test_args())
-        self.assertFalse(c.test_args('a'))
-        self.assertFalse(c.test_args('a', 'b'))
-        self.assertFalse(c.test_args('a', 'b', 'c', 'd'))
+def test_condition_filter_args():
+    c = Condition(lambda a, b, c:True)
+    assert c.test_args('a', 'b', 'c')
+    assert not c.test_args()
+    assert not c.test_args('a')
+    assert not c.test_args('a', 'b')
+    assert not c.test_args('a', 'b', 'c', 'd')
 
-        c2 = Condition(lambda a, b=1:True)
-        self.assertTrue(c2.test_args('a'))
-        self.assertTrue(c2.test_args('a', 'b'))
-        self.assertFalse(c2.test_args('a', 'b', 'c'))
-        self.assertFalse(c2.test_args())
+    c2 = Condition(lambda a, b=1:True)
+    assert c2.test_args('a')
+    assert c2.test_args('a', 'b')
+    assert not c2.test_args('a', 'b', 'c')
+    assert not c2.test_args()
 
-        c3 = Condition(lambda *a: True)
-        self.assertTrue(c3.test_args())
-        self.assertTrue(c3.test_args('a'))
-        self.assertTrue(c3.test_args('a', 'b'))
+    c3 = Condition(lambda *a: True)
+    assert c3.test_args()
+    assert c3.test_args('a')
+    assert c3.test_args('a', 'b')
 
-    def test_and_arg(self):
-        c1 = Condition(lambda a: True)
-        c2 = Condition(lambda a: True)
-        c3 = c1 & c2
+def test_and_arg():
+    c1 = Condition(lambda a: True)
+    c2 = Condition(lambda a: True)
+    c3 = c1 & c2
 
-        self.assertTrue(c3.test_args('a'))
-        self.assertFalse(c3.test_args())
-        self.assertFalse(c3.test_args('a', 'b'))
+    assert c3.test_args('a')
+    assert not c3.test_args()
+    assert not c3.test_args('a', 'b')
 
-    def test_or_arg(self):
-        c1 = Condition(lambda a: True)
-        c2 = Condition(lambda a: True)
-        c3 = c1 | c2
+def test_or_arg():
+    c1 = Condition(lambda a: True)
+    c2 = Condition(lambda a: True)
+    c3 = c1 | c2
 
-        self.assertTrue(c3.test_args('a'))
-        self.assertFalse(c3.test_args())
-        self.assertFalse(c3.test_args('a', 'b'))
+    assert c3.test_args('a')
+    assert not c3.test_args()
+    assert not c3.test_args('a', 'b')
 
-    def test_condition(self):
-        c = Condition(lambda a: a % 2 == 0)
-        self.assertTrue(c(4))
-        self.assertTrue(c(6))
-        self.assertFalse(c(5))
-        self.assertFalse(c(3))
+def test_condition():
+    c = Condition(lambda a: a % 2 == 0)
+    assert c(4)
+    assert c(6)
+    assert not c(5)
+    assert not c(3)
 
-    def test_never(self):
-        self.assertFalse(Never()())
+def test_never():
+    assert not Never()()
 
-    def test_always(self):
-        self.assertTrue(Always()())
+def test_always():
+    assert Always()()
 
-    def test_invert(self):
-        self.assertFalse((~Always())())
-        self.assertTrue((~Never()()))
+def test_invert():
+    assert not (~Always())()
+    assert (~Never()())
 
-        c = ~Condition(lambda: False)
-        self.assertTrue(c())
+    c = ~Condition(lambda: False)
+    assert c()
 
-    def test_or(self):
-        for a in (True, False):
-            for b in (True, False):
-                c1 = Condition(lambda: a)
-                c2 = Condition(lambda: b)
-                c3 = c1 | c2
+def test_or():
+    for a in (True, False):
+        for b in (True, False):
+            c1 = Condition(lambda: a)
+            c2 = Condition(lambda: b)
+            c3 = c1 | c2
 
-                self.assertTrue(isinstance(c3, Filter))
-                self.assertEqual(c3(), a or b)
+            assert isinstance(c3, Filter)
+            assert c3() == a or b
 
-    def test_and(self):
-        for a in (True, False):
-            for b in (True, False):
-                c1 = Condition(lambda: a)
-                c2 = Condition(lambda: b)
-                c3 = c1 & c2
+def test_and():
+    for a in (True, False):
+        for b in (True, False):
+            c1 = Condition(lambda: a)
+            c2 = Condition(lambda: b)
+            c3 = c1 & c2
 
-                self.assertTrue(isinstance(c3, Filter))
-                self.assertEqual(c3(), a and b)
+            assert isinstance(c3, Filter)
+            assert c3() == (a and b)
 
-    def test_cli_filter(self):
-        c1 = Condition(lambda cli: True)
-        self.assertTrue(isinstance(c1, CLIFilter))
-        self.assertFalse(isinstance(c1, SimpleFilter))
+def test_cli_filter():
+    c1 = Condition(lambda cli: True)
+    assert isinstance(c1, CLIFilter)
+    assert not isinstance(c1, SimpleFilter)
 
-        c2 = Condition(lambda: True)
-        self.assertFalse(isinstance(c2, CLIFilter))
-        self.assertTrue(isinstance(c2, SimpleFilter))
+    c2 = Condition(lambda: True)
+    assert not isinstance(c2, CLIFilter)
+    assert isinstance(c2, SimpleFilter)
 
-        c3 = c1 | c2
-        self.assertFalse(isinstance(c3, CLIFilter))
-        self.assertFalse(isinstance(c3, SimpleFilter))
+    c3 = c1 | c2
+    assert not isinstance(c3, CLIFilter)
+    assert not isinstance(c3, SimpleFilter)
 
-        c4 = Condition(lambda cli: True)
-        c5 = Condition(lambda cli: True)
-        c6 = c4 & c5
-        c7 = c4 | c5
-        self.assertTrue(isinstance(c6, CLIFilter))
-        self.assertTrue(isinstance(c7, CLIFilter))
-        self.assertFalse(isinstance(c6, SimpleFilter))
-        self.assertFalse(isinstance(c7, SimpleFilter))
+    c4 = Condition(lambda cli: True)
+    c5 = Condition(lambda cli: True)
+    c6 = c4 & c5
+    c7 = c4 | c5
+    assert isinstance(c6, CLIFilter)
+    assert isinstance(c7, CLIFilter)
+    assert not isinstance(c6, SimpleFilter)
+    assert not isinstance(c7, SimpleFilter)
 
-        c8 = Condition(lambda *args: True)
-        self.assertTrue(isinstance(c8, CLIFilter))
-        self.assertTrue(isinstance(c8, SimpleFilter))
+    c8 = Condition(lambda *args: True)
+    assert isinstance(c8, CLIFilter)
+    assert isinstance(c8, SimpleFilter)
 
-    def test_to_cli_filter(self):
-        f1 = to_cli_filter(True)
-        f2 = to_cli_filter(False)
-        f3 = to_cli_filter(Condition(lambda cli: True))
-        f4 = to_cli_filter(Condition(lambda cli: False))
+def test_to_cli_filter():
+    f1 = to_cli_filter(True)
+    f2 = to_cli_filter(False)
+    f3 = to_cli_filter(Condition(lambda cli: True))
+    f4 = to_cli_filter(Condition(lambda cli: False))
 
-        self.assertTrue(isinstance(f1, CLIFilter))
-        self.assertTrue(isinstance(f2, CLIFilter))
-        self.assertTrue(isinstance(f3, CLIFilter))
-        self.assertTrue(isinstance(f4, CLIFilter))
-        self.assertTrue(f1(None))
-        self.assertFalse(f2(None))
-        self.assertTrue(f3(None))
-        self.assertFalse(f4(None))
+    assert isinstance(f1, CLIFilter)
+    assert isinstance(f2, CLIFilter)
+    assert isinstance(f3, CLIFilter)
+    assert isinstance(f4, CLIFilter)
+    assert f1(None)
+    assert not f2(None)
+    assert f3(None)
+    assert not f4(None)
 
-        self.assertRaises(TypeError, to_cli_filter, 4)
-        self.assertRaises(TypeError, to_cli_filter, Condition(lambda: True))
+    with pytest.raises(TypeError):
+        to_cli_filter(4)
+    with pytest.raises(TypeError):
+        to_cli_filter(Condition(lambda: True))
 
-    def test_to_simple_filter(self):
-        f1 = to_simple_filter(True)
-        f2 = to_simple_filter(False)
-        f3 = to_simple_filter(Condition(lambda: True))
-        f4 = to_simple_filter(Condition(lambda: False))
+def test_to_simple_filter():
+    f1 = to_simple_filter(True)
+    f2 = to_simple_filter(False)
+    f3 = to_simple_filter(Condition(lambda: True))
+    f4 = to_simple_filter(Condition(lambda: False))
 
-        self.assertTrue(isinstance(f1, SimpleFilter))
-        self.assertTrue(isinstance(f2, SimpleFilter))
-        self.assertTrue(isinstance(f3, SimpleFilter))
-        self.assertTrue(isinstance(f4, SimpleFilter))
-        self.assertTrue(f1())
-        self.assertFalse(f2())
-        self.assertTrue(f3())
-        self.assertFalse(f4())
+    assert isinstance(f1, SimpleFilter)
+    assert isinstance(f2, SimpleFilter)
+    assert isinstance(f3, SimpleFilter)
+    assert isinstance(f4, SimpleFilter)
+    assert f1()
+    assert not f2()
+    assert f3()
+    assert not f4()
 
-        self.assertRaises(TypeError, to_simple_filter, 4)
-        self.assertRaises(TypeError, to_simple_filter, Condition(lambda cli: True))
+    with pytest.raises(TypeError):
+        to_simple_filter(4)
+    with pytest.raises(TypeError):
+        to_simple_filter(Condition(lambda cli: True))
 
-    def test_cli_filters(self):
-        self.assertTrue(isinstance(HasArg(), CLIFilter))
-        self.assertTrue(isinstance(HasFocus('BUFFER_NAME'), CLIFilter))
-        self.assertTrue(isinstance(HasSelection(), CLIFilter))
+def test_cli_filters():
+    assert isinstance(HasArg(), CLIFilter)
+    assert isinstance(HasFocus('BUFFER_NAME'), CLIFilter)
+    assert isinstance(HasSelection(), CLIFilter)
