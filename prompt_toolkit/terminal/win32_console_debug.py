5e434bc50bebaf442ee2a8ff7c811d0d154b0dbf
Stephen Rauch <stephen.rauch+github@gmail.com>
2016-12-06 22:16:08 -0800

    Add Ctrl-C handler

--- prompt_toolkit/terminal/win32_console_debug.py
+++ prompt_toolkit/terminal/win32_console_debug.py
@@ -287,10 +287,29 @@ def _attach_console(title=None, create_console=True):
 
         return console_pid, remote_env
 
+    def reattach_sigint_handler():
+        try:
+            import thread
+        except ImportError:
+            import _thread as thread
+
+        # Set our handler for CTRL_C_EVENT. Other control event
+        # types will chain to the next handler.
+        def handler(dw_ctrl_type, hook_sigint=thread.interrupt_main):
+            if dw_ctrl_type == 0:  # CTRL_C_EVENT
+                hook_sigint()
+                return 1  # don't chain to the next handler
+            return 0  # chain to the next handler
+
+        win32api.SetConsoleCtrlHandler(handler, 1)
+
     def prepare_console(console_pid, remote_env):
         # attach to the console
         do_attach_console(console_pid)
         set_console_handles()
+
+        # grab the sigint handler
+        reattach_sigint_handler()
 
         # bring the console to the front
         window_to_foreground(
