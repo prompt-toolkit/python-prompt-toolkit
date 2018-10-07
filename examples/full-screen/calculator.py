a1e7d8f891055e11fb8918e71720ebb3f2db3ba9
Jonathan Slenders <jonathan@slenders.be>
2018-10-07 20:36:01 +0200

    Improved calculator full screen example.
    - Added SearchToolbar.
    - Use accept_handler instead of ENTER key binding.

--- examples/full-screen/calculator.py
+++ examples/full-screen/calculator.py
@@ -12,7 +12,7 @@ from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout.containers import HSplit, Window
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.styles import Style
-from prompt_toolkit.widgets import TextArea
+from prompt_toolkit.widgets import TextArea, SearchToolbar
 
 help_text = """
 Type any expression (e.g. "4 + 4") followed by enter to execute.
@@ -22,14 +22,41 @@ Press Control-C to exit.
 
 def main():
     # The layout.
+    search_field = SearchToolbar()  # For reverse search.
+
     output_field = TextArea(style='class:output-field', text=help_text)
-    input_field = TextArea(height=1, prompt='>>> ', style='class:input-field')
+    input_field = TextArea(
+        height=1, prompt='>>> ', style='class:input-field', multiline=False,
+        wrap_lines=False, search_field=search_field)
 
     container = HSplit([
         output_field,
         Window(height=1, char='-', style='class:line'),
-        input_field
+        input_field,
+        search_field,
     ])
+
+    # Attach accept handler to the input field. We do this by assigning the
+    # handler to the `TextArea` that we created earlier. it is also possible to
+    # pass it to the constructor of `TextArea`.
+    # NOTE: It's better to assign an `accept_handler`, rather then adding a
+    #       custom ENTER key binding. This will automatically reset the input
+    #       field and add the strings to the history.
+    def accept(buff):
+        # Evaluate "calculator" expression.
+        try:
+            output = '\n\nIn:  {}\nOut: {}'.format(
+                input_field.text,
+                eval(input_field.text))  # Don't do 'eval' in real code!
+        except BaseException as e:
+            output = '\n\n{}'.format(e)
+        new_text = output_field.text + output
+
+        # Add text to output buffer.
+        output_field.buffer.document = Document(
+            text=new_text, cursor_position=len(new_text))
+
+    input_field.accept_handler = accept
 
     # The key bindings.
     kb = KeyBindings()
@@ -39,20 +66,6 @@ def main():
     def _(event):
         " Pressing Ctrl-Q or Ctrl-C will exit the user interface. "
         event.app.exit()
-
-    @kb.add('enter', filter=has_focus(input_field))
-    def _(event):
-        try:
-            output = '\n\nIn:  {}\nOut: {}'.format(
-                input_field.text,
-                eval(input_field.text))  # Don't do 'eval' in real code!
-        except BaseException as e:
-            output = '\n\n{}'.format(e)
-        new_text = output_field.text + output
-
-        output_field.buffer.document = Document(
-            text=new_text, cursor_position=len(new_text))
-        input_field.text = ''
 
     # Style.
     style = Style([
