c91a9bd6c0f48d696a3f0bb85cbf98e73197f1f4
Gabi Maeztu <gabriel.maeztu@gmail.com>
2018-10-09 11:29:31 +0200

    Add license to the setup.py
    In order to make automatic checking of license compliance faster, add license to the setup arguments.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     name='prompt_toolkit',
     author='Jonathan Slenders',
     version=get_version('prompt_toolkit'),
+    license='BSD-3-Clause',
     url='https://github.com/jonathanslenders/python-prompt-toolkit',
     description='Library for building powerful interactive command lines in Python',
     long_description=long_description,
