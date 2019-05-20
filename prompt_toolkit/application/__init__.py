c65045aa0914bcbf0c9970a181e57f4c2633f4c7
Jonathan Slenders <jonathan@slenders.be>
2019-05-20 20:54:20 +0200

    Expose get_app_session in aplication/__init__.py

--- prompt_toolkit/application/__init__.py
+++ prompt_toolkit/application/__init__.py
@@ -1,5 +1,11 @@
 from .application import Application
-from .current import create_app_session, get_app, get_app_or_none, set_app
+from .current import (
+    create_app_session,
+    get_app,
+    get_app_or_none,
+    get_app_session,
+    set_app,
+)
 from .dummy import DummyApplication
 from .run_in_terminal import in_terminal, run_in_terminal
 
@@ -8,6 +14,7 @@ __all__ = [
     'Application',
 
     # Current.
+    'get_app_session',
     'create_app_session',
     'get_app',
     'get_app_or_none',
