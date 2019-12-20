9e9aee9dac577f2b53a1323a3fe58eb5693d6c66
Jonathan Slenders <jonathan@slenders.be>
2019-12-20 22:02:38 +0100

    Use stdout as input device if stdin is not a tty, but stdout is.

--- prompt_toolkit/input/defaults.py
+++ prompt_toolkit/input/defaults.py
@@ -12,14 +12,30 @@ __all__ = [
 
 
 def create_input(stdin: Optional[TextIO] = None) -> Input:
-    stdin = stdin or sys.stdin
-
+    """
+    Create the appropriate `Input` object for the current os/environment.
+    """
     if is_windows():
         from .win32 import Win32Input
 
-        return Win32Input(stdin)
+        return Win32Input(stdin or sys.stdin)
     else:
         from .vt100 import Vt100Input
+
+        # If no input TextIO is given, use stdin/stdout.
+        if stdin is None:
+            # Try stdin first, if it's a TTY.
+            if sys.stdin.isatty():
+                stdin = sys.stdin
+            # If stdin is not a TTY, it's possible we're piping something into
+            # stdin. Use stdout instead if stdout is a TTY. (We can actually
+            # use stdout to read input from, this is how a $PAGER works.)
+            elif sys.stdout.isatty():
+                stdin = sys.stdout
+            # If stdout is also not a tty, then use stdin. (This will print a
+            # "Input is not a terminal" warning in `Vt100Input`.)
+            else:
+                stdin = sys.stdin
 
         return Vt100Input(stdin)
 
