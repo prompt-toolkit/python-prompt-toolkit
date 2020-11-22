f77d09c0246571278adebc0140360a8952f20be3
Santos Gallegos <stsewd@protonmail.com>
2020-11-22 11:07:45 -0500

    Don't use classes for tests

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -3,15 +3,16 @@ from prompt_toolkit.layout import to_window
 from prompt_toolkit.widgets import Button
 
 
-class TestButton:
-    def _to_text(self, button: Button) -> str:
-        control = to_window(button).content
-        return fragment_list_to_text(control.text())
+def _to_text(button: Button) -> str:
+    control = to_window(button).content
+    return fragment_list_to_text(control.text())
 
-    def test_defaulf_button(self):
-        button = Button("Exit")
-        assert self._to_text(button) == "<   Exit   >"
 
-    def test_custom_button(self):
-        button = Button("Exit", left_char="[", right_char="]")
-        assert self._to_text(button) == "[   Exit   ]"
+def test_defaulf_button():
+    button = Button("Exit")
+    assert _to_text(button) == "<   Exit   >"
+
+
+def test_custom_button():
+    button = Button("Exit", left_char="[", right_char="]")
+    assert _to_text(button) == "[   Exit   ]"
