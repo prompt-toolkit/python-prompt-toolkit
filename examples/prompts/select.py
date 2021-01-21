48d6ce3d8f9773f7586005f035f211fc79b5e3e5
Yang Liu <liuyangc3@gmail.com>
2021-01-21 18:10:52 +0800

    fix import sort

--- examples/prompts/select.py
+++ examples/prompts/select.py
@@ -6,11 +6,10 @@ An example of select prompt.
 from prompt_toolkit.application import Application
 from prompt_toolkit.filters import IsDone
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.layout.containers import Window
+from prompt_toolkit.layout.containers import ConditionalContainer, HSplit, Window
 from prompt_toolkit.layout.controls import FormattedTextControl
-from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.layout.dimension import Dimension
-from prompt_toolkit.layout.containers import ConditionalContainer, HSplit
+from prompt_toolkit.layout.layout import Layout
 
 
 class SelectControl(FormattedTextControl):
