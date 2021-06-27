f02437ae9cbe085c4ee314ebb9a01f1d922de74c
Noortheen Raja <jnoortheen@gmail.com>
2021-06-27 16:09:55 +0530

    chore: add py3.10 to ci

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python",
         "Topic :: Software Development",
