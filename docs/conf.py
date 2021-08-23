279720474de378d6a970efc0858b595d798c5163
Kian Cross <kian@kiancross.co.uk>
2021-08-23 01:06:29 +0100

    Update to Sphinx 4.1

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import sys
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.graphviz"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 # The suffix of source filenames.
 source_suffix = ".rst"
@@ -139,7 +139,7 @@ html_logo = "images/logo_400px.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
