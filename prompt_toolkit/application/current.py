1c833d8031e0ba344c3c7d94847d8e67f877c515
Jonathan Slenders <jonathan@slenders.be>
2022-02-03 22:31:59 +0100

    Added create_app_session_from_tty.
    A utility to quickly set up input/output attached to the TTY.

--- prompt_toolkit/application/current.py
+++ prompt_toolkit/application/current.py
@@ -20,6 +20,7 @@ __all__ = [
     "get_app_or_none",
     "set_app",
     "create_app_session",
+    "create_app_session_from_tty",
 ]
 
 
@@ -168,3 +169,29 @@ def create_app_session(
         yield session
     finally:
         _current_app_session.reset(token)
+
+
+@contextmanager
+def create_app_session_from_tty() -> Generator[AppSession, None, None]:
+    """
+    Create `AppSession` that always prefers the TTY input/output.
+
+    Even if `sys.stdin` and `sys.stdout` are connected to input/output pipes,
+    this will still use the terminal for interaction (because `sys.stderr` is
+    still connected to the terminal).
+
+    Usage::
+
+        from prompt_toolkit.shortcuts import prompt
+
+        with create_app_session_from_tty():
+            prompt('>')
+    """
+    from prompt_toolkit.input.defaults import create_input
+    from prompt_toolkit.output.defaults import create_output
+
+    input = create_input(always_prefer_tty=True)
+    output = create_output(always_prefer_tty=True)
+
+    with create_app_session(input=input, output=output) as app_session:
+        yield app_session
