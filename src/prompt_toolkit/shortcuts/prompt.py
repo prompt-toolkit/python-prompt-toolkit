2a5fe53918c0fe64879aca4292c9306734064c8a
Noortheen Raja <jnoortheen@gmail.com>
2022-03-12 23:41:07 +0530

    fix: right prompt not aligned correctly
    closes #1241

--- src/prompt_toolkit/shortcuts/prompt.py
+++ src/prompt_toolkit/shortcuts/prompt.py
@@ -689,7 +689,7 @@ class PromptSession(Generic[_T]):
                         # The right prompt.
                         Float(
                             right=0,
-                            bottom=0,
+                            top=0,
                             hide_when_covering_content=True,
                             content=_RPrompt(lambda: self.rprompt),
                         ),
