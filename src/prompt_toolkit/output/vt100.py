prompt-toolkit/python-prompt-toolkit

commit 2d4e7e87f25e5d9bbd5c10f5e7b66c5d7c7a77f6

Added enable_cpr parameter to Vt100_Output, TelnetServer and PromptToolkitSSHServer.

src/prompt_toolkit/output/vt100.py
@@ -400,6 +400,13 @@ class Vt100_Output(Output):
     :param get_size: A callable which returns the `Size` of the output terminal.
     :param stdout: Any object with has a `write` and `flush` method + an 'encoding' property.
     :param term: The terminal environment variable. (xterm, xterm-256color, linux, ...)
+    :param enable_cpr: When `True` (the default), send "cursor position
+        request" escape sequences to the output in order to detect the cursor
+        position. That way, we can properly determine how much space there is
+        available for the UI (especially for drop down menus) to render. The
+        `Renderer` will still try to figure out whether the current terminal
+        does respond to CPR escapes. When `False`, never attempt to send CPR
+        requests.
     """
 
     # For the error messages. Only display "Output is not a terminal" once per
@@ -413,6 +420,7 @@ class Vt100_Output(Output):
         term: Optional[str] = None,
         default_color_depth: Optional[ColorDepth] = None,
         enable_bell: bool = True,
+        enable_cpr: bool = False,
     ) -> None:
 
         assert all(hasattr(stdout, a) for a in ("write", "flush"))
@@ -423,6 +431,7 @@ class Vt100_Output(Output):
         self._get_size = get_size
         self.term = term
         self.enable_bell = enable_bell
+        self.enable_cpr = enable_cpr
 
         # Cache for escape codes.
         self._escape_code_caches: Dict[ColorDepth, _EscapeCodeCache] = {
@@ -703,6 +712,9 @@ class Vt100_Output(Output):
 
     @property
     def responds_to_cpr(self) -> bool:
+        if not self.enable_cpr:
+            return False
+
         # When the input is a tty, we assume that CPR is supported.
         # It's not when the input is piped from Pexpect.
         if os.environ.get("PROMPT_TOOLKIT_NO_CPR", "") == "1":

src/prompt_toolkit/contrib/ssh/server.py
@@ -18,9 +18,13 @@ __all__ = ["PromptToolkitSSHSession", "PromptToolkitSSHServer"]
 
 class PromptToolkitSSHSession(asyncssh.SSHServerSession):  # type: ignore
     def __init__(
-        self, interact: Callable[["PromptToolkitSSHSession"], Awaitable[None]]
+        self,
+        interact: Callable[["PromptToolkitSSHSession"], Awaitable[None]],
+        *,
+        enable_cpr: bool,
     ) -> None:
         self.interact = interact
+        self.enable_cpr = enable_cpr
         self.interact_task: Optional[asyncio.Task[None]] = None
         self._chan: Optional[Any] = None
         self.app_session: Optional[AppSession] = None
@@ -85,7 +89,9 @@ class PromptToolkitSSHSession(asyncssh.SSHServerSession):  # type: ignore
 
         term = self._chan.get_terminal_type()
 
-        self._output = Vt100_Output(self.stdout, self._get_size, term=term)
+        self._output = Vt100_Output(
+            self.stdout, self._get_size, term=term, enable_cpr=self.enable_cpr
+        )
 
         with create_pipe_input() as self._input:
             with create_app_session(input=self._input, output=self._output) as session:
@@ -145,16 +151,25 @@ class PromptToolkitSSHServer(asyncssh.SSHServer):  # type: ignore
             )
         )
         loop.run_forever()
+
+    :param enable_cpr: When `True`, the default, try to detect whether the SSH
+        client runs in a terminal that responds to "cursor position requests".
+        That way, we can properly determine how much space there is available
+        for the UI (especially for drop down menus) to render.
     """
 
     def __init__(
-        self, interact: Callable[[PromptToolkitSSHSession], Awaitable[None]]
+        self,
+        interact: Callable[[PromptToolkitSSHSession], Awaitable[None]],
+        *,
+        enable_cpr: bool = True,
     ) -> None:
         self.interact = interact
+        self.enable_cpr = enable_cpr
 
     def begin_auth(self, username: str) -> bool:
         # No authentication.
         return False
 
     def session_requested(self) -> PromptToolkitSSHSession:
-        return PromptToolkitSSHSession(self.interact)
+        return PromptToolkitSSHSession(self.interact, enable_cpr=self.enable_cpr)

src/prompt_toolkit/contrib/telnet/server.py
@@ -132,6 +132,7 @@ class TelnetConnection:
         encoding: str,
         style: Optional[BaseStyle],
         vt100_input: PipeInput,
+        enable_cpr: bool = True,
     ) -> None:
 
         self.conn = conn
@@ -143,7 +144,8 @@ class TelnetConnection:
         self._closed = False
         self._ready = asyncio.Event()
         self.vt100_input = vt100_input
-        self.vt100_output = None
+        self.enable_cpr = enable_cpr
+        self.vt100_output: Optional[Vt100_Output] = None
 
         # Create "Output" object.
         self.size = Size(rows=40, columns=79)
@@ -169,7 +171,9 @@ class TelnetConnection:
 
         def ttype_received(ttype: str) -> None:
             """TelnetProtocolParser 'ttype_received' callback"""
-            self.vt100_output = Vt100_Output(self.stdout, get_size, term=ttype)
+            self.vt100_output = Vt100_Output(
+                self.stdout, get_size, term=ttype, enable_cpr=enable_cpr
+            )
             self._ready.set()
 
         self.parser = TelnetProtocolParser(data_received, size_received, ttype_received)
@@ -274,6 +278,7 @@ class TelnetServer:
         interact: Callable[[TelnetConnection], Awaitable[None]] = _dummy_interact,
         encoding: str = "utf-8",
         style: Optional[BaseStyle] = None,
+        enable_cpr: bool = True,
     ) -> None:
 
         self.host = host
@@ -281,6 +286,7 @@ class TelnetServer:
         self.interact = interact
         self.encoding = encoding
         self.style = style
+        self.enable_cpr = enable_cpr
         self._application_tasks: List[asyncio.Task[None]] = []
 
         self.connections: Set[TelnetConnection] = set()
@@ -345,6 +351,7 @@ class TelnetServer:
                         encoding=self.encoding,
                         style=self.style,
                         vt100_input=vt100_input,
+                        enable_cpr=self.enable_cpr,
                     )
                     self.connections.add(connection)
 

commit 783d8a9fbefbe276810e7852456722f8910edd95

Fixed bug: `enable_cpr` should be True by default.
Bug was introduced in previous commit (2d4e7e87f25e5d9bbd5c10f5e7b66c5d7c7a77f6).

src/prompt_toolkit/output/vt100.py
@@ -420,7 +420,7 @@ class Vt100_Output(Output):
         term: Optional[str] = None,
         default_color_depth: Optional[ColorDepth] = None,
         enable_bell: bool = True,
-        enable_cpr: bool = False,
+        enable_cpr: bool = True,
     ) -> None:
 
         assert all(hasattr(stdout, a) for a in ("write", "flush"))