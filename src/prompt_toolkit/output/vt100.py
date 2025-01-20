prompt-toolkit/python-prompt-toolkit

commit f2c7a159efcc2ccecf2ce1cf30fd247bb572f3d9

Fix cursor that remains in hidden state when application exits. (#1925)
When we have an application that doesn't show the cursor and `erase_when_done`
is being used, the cursor remained hidden after the application terminated.

src/prompt_toolkit/renderer.py
@@ -257,7 +257,7 @@ def _output_screen_diff(
     # give weird artifacts on resize events.)
     reset_attributes()
 
-    if screen.show_cursor or is_done:
+    if screen.show_cursor:
         output.show_cursor()
 
     return current_pos, last_style
@@ -416,6 +416,7 @@ class Renderer:
             self._bracketed_paste_enabled = False
 
         self.output.reset_cursor_shape()
+        self.output.show_cursor()
 
         # NOTE: No need to set/reset cursor key mode here.
 

commit 60a6e5f74f18bffbce82f7d21c48f0d123d1b2e0

Optimize rendering. Don't output escape codes for hiding/showing the cursor if not needed.
This fixes a regression that was introduced in
https://github.com/prompt-toolkit/python-prompt-toolkit/pull/1925

src/prompt_toolkit/output/vt100.py
@@ -436,6 +436,11 @@ class Vt100_Output(Output):
         # default, we don't change them.)
         self._cursor_shape_changed = False
 
+        # Don't hide/show the cursor when this was already done.
+        # (`None` means that we don't know whether the cursor is visible or
+        # not.)
+        self._cursor_visible: bool | None = None
+
     @classmethod
     def from_pty(
         cls,
@@ -651,10 +656,14 @@ class Vt100_Output(Output):
             self.write_raw("\x1b[%iD" % amount)
 
     def hide_cursor(self) -> None:
-        self.write_raw("\x1b[?25l")
+        if self._cursor_visible in (True, None):
+            self._cursor_visible = False
+            self.write_raw("\x1b[?25l")
 
     def show_cursor(self) -> None:
-        self.write_raw("\x1b[?12l\x1b[?25h")  # Stop blinking cursor and show.
+        if self._cursor_visible in (False, None):
+            self._cursor_visible = True
+            self.write_raw("\x1b[?12l\x1b[?25h")  # Stop blinking cursor and show.
 
     def set_cursor_shape(self, cursor_shape: CursorShape) -> None:
         if cursor_shape == CursorShape._NEVER_CHANGE:

src/prompt_toolkit/renderer.py
@@ -353,6 +353,11 @@ class Renderer:
         self.mouse_support = to_filter(mouse_support)
         self.cpr_not_supported_callback = cpr_not_supported_callback
 
+        # TODO: Move following state flags into `Vt100_Output`, similar to
+        #       `_cursor_shape_changed` and `_cursor_visible`. But then also
+        #       adjust the `Win32Output` to not call win32 APIs if nothing has
+        #       to be changed.
+
         self._in_alternate_screen = False
         self._mouse_support_enabled = False
         self._bracketed_paste_enabled = False