f04219e12c9327b38dec023b1e5c2d7380811835
Kevin Van Brunt <kmvanbrunt@gmail.com>
2026-03-15 16:56:08 -0400

    Fix vt100 6x6x6 color cube range and missing grayscale shades (232, 254-255) (#2055)

--- src/prompt_toolkit/output/vt100.py
+++ src/prompt_toolkit/output/vt100.py
@@ -210,17 +210,17 @@ class _256ColorCache(Dict[Tuple[int, int, int], int]):
         colors.append((0x00, 0xFF, 0xFF))  # 14
         colors.append((0xFF, 0xFF, 0xFF))  # 15
 
-        # colors 16..232: the 6x6x6 color cube
+        # colors 16..231: the 6x6x6 color cube
         valuerange = (0x00, 0x5F, 0x87, 0xAF, 0xD7, 0xFF)
 
-        for i in range(217):
+        for i in range(216):
             r = valuerange[(i // 36) % 6]
             g = valuerange[(i // 6) % 6]
             b = valuerange[i % 6]
             colors.append((r, g, b))
 
-        # colors 233..253: grayscale
-        for i in range(1, 22):
+        # colors 232..255: grayscale
+        for i in range(24):
             v = 8 + i * 10
             colors.append((v, v, v))
 
