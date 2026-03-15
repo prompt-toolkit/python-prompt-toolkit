4fecdac44235539d41a91edeb7f4739cc50fa660
Kevin Van Brunt <kmvanbrunt@gmail.com>
2026-03-14 23:04:35 -0400

    Correct 6x6x6 color cube range and missing grayscale shades (232, 254-255)

--- tests/test_vt100_output.py
+++ tests/test_vt100_output.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from prompt_toolkit.output.vt100 import _get_closest_ansi_color
+from prompt_toolkit.output.vt100 import _256_colors, _get_closest_ansi_color
 
 
 def test_get_closest_ansi_color():
@@ -18,3 +18,14 @@ def test_get_closest_ansi_color():
     assert _get_closest_ansi_color(0, 255, 10) == "ansibrightgreen"
 
     assert _get_closest_ansi_color(220, 220, 100) == "ansiyellow"
+
+
+def test_256_colors():
+    # 6x6x6 cube
+    assert _256_colors[(0, 0, 0)] == 16  # First color in cube
+    assert _256_colors[(255, 255, 255)] == 231  # Last color in cube
+    assert _256_colors[(95, 95, 95)] == 59  # Verifies a color between the boundaries
+
+    # Grayscale
+    assert _256_colors[(8, 8, 8)] == 232  # First grayscale level
+    assert _256_colors[(238, 238, 238)] == 255  # Last grayscale level
