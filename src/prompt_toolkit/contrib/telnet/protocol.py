9f0d2e97c4d67f8f12cd9f73fb79e28f89d71a3e
Jonathan Slenders <jonathan@slenders.be>
2026-07-26 20:54:17 +0200

    Handle ascii decode errors in terminal type decoding of telnet protocol.

--- src/prompt_toolkit/contrib/telnet/protocol.py
+++ src/prompt_toolkit/contrib/telnet/protocol.py
@@ -135,7 +135,7 @@ class TelnetProtocolParser:
         """
         subcmd, data = data[0:1], data[1:]
         if subcmd == IS:
-            ttype = data.decode("ascii")
+            ttype = data.decode("ascii", errors="replace")
             self.ttype_received_callback(ttype)
         else:
             logger.warning("Received a non-IS terminal type Subnegotiation")
